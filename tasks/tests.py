e05fc571624a1fcdd99b3c54e2803c14a4fa201a
https://koger1O@bitbucket.org/SHOT/SHOT_Assignment1.git <https://[gergo_kocsis@outlook.com]@bitbucket.org/[SHOT]/[SHOT_assignment1].git>
2025-11-12 20:08:59 +0000

    test test executing

--- tasks/tests.py
+++ tasks/tests.py
@@ -1,3 +1,10 @@
-from django.test import TestCase
+# from django.test import TestCase
+import pytest
+from playwright.sync_api import Page, expect
 
-# Create your tests here.
+def test_login(page: Page):
+    page.goto("http://127.0.0.1:8000/login/")
+    page.fill('input[name="username"]', 'user1')
+    page.fill('input[name="password"]', 'testpass123')
+    page.click('button[type="submit"]')
+    expect(page).to_have_url("http://127.0.0.1:8000/dashboard/")

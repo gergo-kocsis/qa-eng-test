e5d532fe8890aa9e831fd26a1e4bf7251a9f938a
https://koger1O@bitbucket.org/SHOT/SHOT_Assignment1.git <https://[gergo_kocsis@outlook.com]@bitbucket.org/[SHOT]/[SHOT_assignment1].git>
2025-11-12 20:47:06 +0000

    added login check

--- QA-Automation/helpers.py
+++ QA-Automation/helpers.py
@@ -1,6 +1,6 @@
 from playwright.sync_api import Page, expect
 
-def login(page: Page, username, password):
+def login(page: Page, username, password, validLogin = True):
     # Ensure we are on the right page
     expect(page).to_have_title('Login - Todo App')
 
@@ -15,4 +15,14 @@ def login(page: Page, username, password):
 
     # Submit login
     loginB.click()
-    
+
+    """
+        NOTE: I want to validate successful login attempts, but I may also want to test invalid attempts too. 
+        Instead of having a separate function for that, or retyping the login logic for failing paths, I
+        added this branch to allow for both.
+    """
+    if(validLogin):
+        expect(page).to_have_title('Dashboard - Todo App')
+
+    else:
+        expect(page.get_by_text("Invalid username or password.")).to_be_visible()
--- QA-Automation/tests.py
+++ QA-Automation/tests.py
@@ -4,7 +4,8 @@ from playwright.sync_api import Page, expect
 from helpers import login
 
 def test_login(page: Page):
+    # TODO: before hook
     page.goto("http://127.0.0.1:8000/login/")
 
-    # Ensure we are on the right page
+    login(page, "user1", "hello", False)
     login(page, "user1", "password123")
